f8972a39c6fe67f789ae5ee809c3ebc057b86f42
frinfo702 <121755762+frinfo702@users.noreply.github.com>
2024-11-25 20:55:58 +0900

    改善: wordDictをsetに変換し、最大単語長を考慮してループの範囲を最適化

--- 139-word-break/step2.py
+++ 139-word-break/step2.py
@@ -3,9 +3,11 @@ class Solution:
         num_partition = len(s) + 1 # sの各文字間の仕切りの数
         can_break = [False] * num_partition # 仕切りの最後で分割可能(True)なら最終的に分割可能
         can_break[0] = True # 仕切りの先頭は任意の文字列で分割可能
-        
+        words = set(wordDict)        
+        max_word_length = max(len(word) for word in words)
+
         for end_i in range(1, len(s) + 1):
-            for start_i in range(end_i):
+            for start_i in range(max(0, end_i - max_word_length),end_i):
                 substring = s[start_i:end_i]
                 # 新たな部分文字列が見つかり、その直前の仕切りで分割可能
                 if can_break[start_i] and substring in wordDict:
--- 139-word-break/step3.py
+++ 139-word-break/step3.py
@@ -3,12 +3,13 @@ class Solution:
         num_partition = len(s) + 1
         can_break = [False] * num_partition
         can_break[0] = True
-        wordSet = set(wordDict)
+        words = set(wordDict)
+        max_word_length = max(len(word) for word in words)
 
         for end_i in range(1, len(s) + 1):
-            for start_i in range(end_i):
+            for start_i in range(max(0, end_i - max_word_length), end_i):
                 substring = s[start_i:end_i]
-                if can_break[start_i] and substring in wordSet:
+                if can_break[start_i] and substring in words:
                     can_break[end_i] = True
         
         return can_break[-1]
