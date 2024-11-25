14735207a8f1c8850b188461deccde5b6b79afd5
frinfo702 <121755762+frinfo702@users.noreply.github.com>
2024-11-25 20:51:15 +0900

    改善: wordDictをsetに変換し、最大単語長を考慮してループの範囲を最適化

--- 139-word-break/step1.py
+++ 139-word-break/step1.py
@@ -2,9 +2,11 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         dp = [False] * (len(s) + 1) # 動的計画法のメモのため
         dp[0] = True # len(s)が0なら必ず分割可能
-        
+        words = set(wordDict)
+        max_word_length = max(len(word) for word in words)
+
         for i in range(1, len(s) + 1):
-            for j in range(i):
+            for j in range(max(0, i - max_word_length) , i):
                 if dp[j] and s[j:i] in wordDict:
                     dp[i] = True
                     break
