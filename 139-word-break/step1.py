f1f571c81d0d433cf0e23db978f58c00427cd0c5
frinfo702 <121755762+frinfo702@users.noreply.github.com>
2024-11-25 20:41:45 +0900

    改善: +の前後にスペースを追加

--- 139-word-break/step1.py
+++ 139-word-break/step1.py
@@ -1,9 +1,9 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp = [False] * (len(s)+1) # 動的計画法のメモのため
+        dp = [False] * (len(s) + 1) # 動的計画法のメモのため
         dp[0] = True # len(s)が0なら必ず分割可能
         
-        for i in range(1, len(s)+1):
+        for i in range(1, len(s) + 1):
             for j in range(i):
                 if dp[j] and s[j:i] in wordDict:
                     dp[i] = True
