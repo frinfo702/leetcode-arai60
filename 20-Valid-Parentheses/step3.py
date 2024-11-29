fa9caf30215d21c481d642ba170098212c32a674
frinfo702 <121755762+frinfo702@users.noreply.github.com>
2024-11-29 19:05:10 +0900

    条件分岐を簡略化し、不要なelse文を削除してコードの可読性を向上

--- 20-Valid-Parentheses/step3.py
+++ 20-Valid-Parentheses/step3.py
@@ -7,11 +7,10 @@ class Solution:
             if char in brackets_pair:
                 brackets_stack.append(char)
                 continue
-            else:
-                if not brackets_stack:
-                    return False
-                if brackets_pair[brackets_stack[-1]] != char:
-                    return False
-                brackets_stack.pop()
+            if not brackets_stack:
+                return False
+            if brackets_pair[brackets_stack[-1]] != char:
+                return False
+            brackets_stack.pop()
 
         return not brackets_stack
